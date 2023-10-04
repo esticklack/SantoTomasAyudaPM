610fb1c51bcb740a4424e2b026f7742998f0d78e
esticklack <62732687+esticklack@users.noreply.github.com>
2023-10-04 02:30:19 -0300

    Agregados
    Cerrar sesion y más cosas

--- app/src/main/java/gi/stomasayuda/pm/HomeActivity.java
+++ app/src/main/java/gi/stomasayuda/pm/HomeActivity.java
@@ -5,11 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+<<<<<<< Updated upstream
+=======
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+>>>>>>> Stashed changes
 
 public class HomeActivity extends AppCompatActivity {
 
     Button btnBuscarAula;
     Button btnAgendarSala;
+<<<<<<< Updated upstream
+=======
+    ImageView btnConfiguration;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +36,49 @@ public class HomeActivity extends AppCompatActivity {
 
       
         btnBuscarAula = findViewById(R.id.btnBuscarAula);
+<<<<<<< Updated upstream
+=======
+        btnAgendarSala = findViewById(R.id.btnAgendarSala);
+        btnConfiguration = findViewById(R.id.imgConfiguration);
+
+        btnConfiguration.setOnClickListener(view -> irAConfiguracion());
+
+        btnBuscarAula.setOnClickListener(view -> irABuscarAula());
+
+        btnAgendarSala.setOnClickListener(view -> irAgendarSala());
+
+
+
+
+        if (user != null) {
+            String email = user.getEmail();
+
+            db.collection("usuarios")
+                    .whereEqualTo("correo", email)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String nombre = document.getString("nombre");
+                                TextView textViewNombre = findViewById(R.id.BienvenidaNombreApellido);
+
+
+                                if (nombre.toLowerCase().endsWith("o")) {
+                                    textViewNombre.setText("Bienvenido "+ nombre);
+                                } else if (nombre.toLowerCase().endsWith("a")) {
+                                    textViewNombre.setText("Bienvenida "+ nombre);
+                                } else {
+                                    // Por defecto, si no termina en "o" ni en "a", simplemente decimos "Bienvenido/a" seguido del nombre.
+                                    textViewNombre.setText("Bienvenido/a "+ nombre);
+                                }
+
+                            }
+                        } else {
+                            Log.d("TAG", "Error obteniendo la presentación.: ", task.getException());
+                        }
+                    });
+        }
+>>>>>>> Stashed changes
 
         btnBuscarAula.setOnClickListener(view -> {
             irABuscarAula();
@@ -30,9 +92,25 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void irAgendarSala(){
+<<<<<<< Updated upstream
         Intent I = new Intent(HomeActivity.this, AuthActivity.class);
+=======
+        Intent I = new Intent(HomeActivity.this, SalasDeEstudiosActivity.class);
+>>>>>>> Stashed changes
         startActivity(I);
-
-
     }
+<<<<<<< Updated upstream
+=======
+
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+    }
+
+    public void irAConfiguracion(){
+        Intent I = new Intent(HomeActivity.this, ConfigActivity.class);
+        startActivity(I);
+    }
+
+>>>>>>> Stashed changes
 }
